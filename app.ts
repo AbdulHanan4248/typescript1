cfc99c7b0fe8786c18a175e366fe5d006984ba58
AbdulHanan4248 <abdulhananrauf4248@gmail.com>
2022-09-01 02:07:24 +0900

    your comment

---
+++ app.ts
@@ -0,0 +1,349 @@
+// var user = 'sdf'
+// console.log(user)
+// var number = 10
+// console.log(number)
+// var firstnumber = 20
+// var secondnumber = 30
+// var thirdnumber = 50
+// var fourthnumber = firstnumber + thirdnumber
+// console.log(fourthnumber)
+// var fifthnumber = secondnumber + firstnumber
+// console.log(fifthnumber);
+// var sixthnumber = fifthnumber - thirdnumber
+// console.log(sixthnumber);
+// var seventhnumber = ++sixthnumber
+// console.log(seventhnumber);
+// var eightnumber = secondnumber++
+// console.log(eightnumber);
+// var number1:number = 20;
+// var number2:number = 10;
+// var number3:number = 30;
+// var number4:number = 40;
+// var result:boolean = false ||true && false || true
+// console.log("result", result);
+// var tempC:number = 42;
+// var tempF:number = (tempC *9/5)-42
+// console.log("Temp", tempF);
+//  var number1:number = 23;
+//  var number2:number = 33;
+//  var number3:number = 43;
+//  var number5:number = number1+number3
+//  var result  = !(false || true && true && false || false)
+// console.log("result",  result);
+// var number1:number = 24
+// var number2:number = 30
+// var number3:number = 50
+// var number4:number = number1+number3
+// console.log(number4);
+// var number5:number = number2-number4
+// console.log(number5);
+// var number6:number = --number3
+// console.log(number6);
+// var number7:number = number6-number1*number3/number4
+// console.log(number7);
+// var number8:number = number7=+number4
+// console.log(number8);
+// var number9:number = number7/=number4
+// console.log(number9);
+// var number10:number = number5*=number1
+// console.log(number10);
+// var number11:number = number10 % number2
+// console.log(number11);
+// var number12:number = number11--
+// console.log(number12);
+// var number13:number = number12++
+// console.log(number13);
+// var number14:number = ++number13
+// console.log(number14);
+// var result:boolean = number14>=number3
+// var result:boolean = number10<=number2
+// var result:boolean = number13!=number14
+// console.log("result" , result);
+
+// var firstnumber :number = 23
+// var secondnumber:number = 60
+// var thirdnumber:number = 23
+// var result:boolean = firstnumber >= thirdnumber
+// console.log(result);
+  
+// var inputnumber:number = 0;
+// var statementAnswer:boolean = inputnumber==0;
+// if(inputnumber==0){
+//     console.log("number is zero");}
+//     if(number %2 ==0){
+//         console.log("number id even");
+//     }
+//         else{
+//             console.log("number is odd");
+//         }
+//             var line1:number = 1
+//             var line2:number = 2
+//             var line3:number = 3
+//             var line4:number = 4
+//             var line5:number = 5
+//             var line6:number = 6
+//             if(true){
+//                 console.log(line5)
+//                 console.log(line1);
+//                 console.log(line3);
+//                 console.log(line4);
+//             }
+//             if(true){
+//                 console.log(line5);
+//                 console.log(line6);
+//             }
+//             var userage = 16;
+//             var gender = 'male'
+//             if(userage >=18){
+//                 console.log('male is adult');    
+//             }
+//              else{
+//                 console.log("male is not adult");
+//              } 
+//              var x: number = 30
+//              var y:number = 25
+//              if(x>y){
+//                 console.log("x is greater than y");
+                
+//              } 
+//              else{
+//                 console.log("x is less than y");
+//              }
+//              var num1: number = 10;
+// var num2: number = 10;
+//              if (num1 > num2) {
+//                 console.log("num1 is greater than num2");
+//               } else if (num2 == num1) {
+//                 console.log("num2 is equal num1");
+//               } else {
+//                 console.log("num2 is greater than num1");
+//               }
+//              var grade:string = 'F';
+//              if(grade== "A"){
+//                 console.log("grade is A");
+                
+//              }
+//              else if(grade== "B"){
+//                 console.log("grade is B");
+                
+//              }
+//              else if(grade== "C"){
+//                 console.log("grade is C");
+                
+//              }
+//               else if(grade== "F"){
+//                 console.log("grade is F");
+                
+//               }  
+//                 var num:number = 85;
+                
+
+//                 if(num>=90){
+//                     console.log("Grade is +A");
+                    
+//                 }
+//                 else if(num>=80){
+//                     console.log("Grade is A");
+                    
+//                 }
+//                 else if(num>=70){
+//                     console.log("Grade is B");
+                    
+//                 }
+//                 else if(num>=60){
+//                     console.log("Grade is C");
+                    
+//                 }
+//                 else if(num>=50){
+
+//                     console.log("Grade is D");
+                    
+//                 }
+//                 else if(num>=40){
+//                     console.log("Grade is F");
+                    
+//                 }
+                
+
+//                 switch (grade){
+//                     case "A": {
+//                         console.log("grade is A");
+//                         break;
+                        
+//                     }
+//                     case "B": {
+//                         console.log("grade is B");
+//                         break;   
+//                     }
+//                     case "C": {
+//                         console.log("grade is C");
+//                         break;
+//                     }
+//                     case "D": {
+//                         console.log("grade is D");
+//                         break;
+//                     }
+//                     }
+//                     if(inputnumber%2==0){
+//                         console.log("number is even");
+                        
+//                     }
+//                     var num:number = 0
+//                     if(num>=0){
+//                         console.log("number is positive");
+                        
+//                     }
+//                     else{
+//                         console.log("number is negitve");
+                        
+//                     }
+//                     var num:number = 11;
+//                 if(num>=0) {
+//                     console.log("munber is maxium");
+                    
+//                 }
+            
+//                  var string:string = "A to z" 
+//                  var number = 0                 
+//                  {
+//                     console.log("This is alphabet");
+                    
+//                 }
+ //               var letter: string | null = prompt("Enter any character and check this is uppercase or lowercase.")
+                    
+//                 if(letter == letter?.toUpperCase()){
+//                     console.log("character is uppercase");
+//                 }
+//                 if(letter == letter?.toLowerCase()){
+//                     console.log("character is lowercase");
+                    
+//                 }
+//                
+//                 var inputnumber:number = 2000;
+                      
+//                 if(inputnumber %4==0){
+//                     console.log("This is leap year");
+                    
+//                 }
+//                 else{
+//                     console.log("This is not leap year");
+                    
+//                 }
+//                 // 65 to 90 97to 122  Alphabet
+//                 // 31 to 47 special 58 to 64 Character
+//                 // 48 to 57 digit
+
+//                 
+                    var units = 256;
+                    //var amount = 0;
+                    //if(units >= 0 &&units <= 50){
+                       // amount = units *0.50
+                       
+                        
+                   // }
+                   // else if (units>=50 && units<=100) {
+                       // amount = units*0.75
+                        
+                        
+                    //} 
+                  //  else if (units >=100 && units<=200) {
+                      //  amount = units*1
+                      
+                        
+                  //  }
+                   // else if (units >=200 && units <=300 ){
+                      //  amount = units*2
+                   
+                    
+                        
+                   // }
+                   // console.log(amount);
+
+                    var basicsalary;
+                    var da;
+                    var hra;
+                    
+            
+
+                   if(basicsalary>=10000) {
+                         da = basicsalary*0.8;
+                         hra = basicsalary*0.2 
+                    }  
+                    else if(basicsalary<=20000) {
+                        da = basicsalary*0.9;
+                        hra = basicsalary*0.25;
+                    } 
+                    else if(basicsalary<=30000) {
+                        da = basicsalary*0.95;
+                        hra = basicsalary*0.3;
+                    } 
+                    console.log(basicsalary);
+                    
+
+                    alert("This is our country")
+
+                   // var number = 22
+                   // if(number %11==0 && number %5==0 ) {
+                     //   console.log("Number is divisible by 11 and 5 ");
+                        
+                  //  }
+
+                   // else
+            
+                    // {
+                      //  console.log("number is not divisible by 11 and 5");
+                        
+                   // }
+
+                   var string = "h"
+                   var string = "I"
+
+                   if(string=='a'|| string=='i'|| string=='o' || string=='u' 
+                   || string=="e" ){
+                    
+                   }
+
+                   else if(string=='A'|| string=='I'|| string=='O' || string=='U' 
+                   || string=="E" ){
+                    console.log("It is vowel words");
+                    
+                   }
+                   else{
+                    console.log("This is consonat");
+                    
+                   }
+
+
+                
+
+                       
+                                 
+                        
+                    
+                    
+
+
+                
+                    
+                
+//for (var i = 1; i<=10; i++) {
+   
+    //let hanan:string =`10 *  ${i} = ${10*i}`
+    //console.log(hanan);
+    
+    
+//}
+                 
+                    
+                
+                    
+                 
+                    
+                
+                
+                
+
+                
+            
+                
+            
